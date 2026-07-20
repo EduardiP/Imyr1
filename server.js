d2f13e7c98d84d711c5126b8d7b6abfe3e5e3840
EduardiP <blijonline010@gmail.com>
2026-07-20 21:02:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -737,7 +737,6 @@ app.get('/api/admin/biznes/:id', iAdmin, async (req, res) => {
 // --- Faqet ---
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
-app.get('/app', (req, res) => res.sendFile(path.join(__dirname, 'public', 'app.html')));
 app.get('/privacy', (req, res) => res.sendFile(path.join(__dirname, 'public', 'privacy.html')));
 app.get('/terms', (req, res) => res.sendFile(path.join(__dirname, 'public', 'terms.html')));
 app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'public', 'contact.html')));
