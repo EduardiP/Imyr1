5c696c693b1ac3249a3e388adfd4ca719dfb2e15
EduardiP <blijonline010@gmail.com>
2026-07-31 14:41:07 +0200

    Update demo-matje.js

--- demo-matje.js
+++ demo-matje.js
@@ -46,7 +46,7 @@ function layout(titulli, trupi){
 <style>${CSS}</style>
 </head>
 <body>
-
+<script src="https://phronexusai.com/imyr.js" data-key="imyr_4f210452ce2b25809305d5a7"></script>
 <header>
   <span class="lg">Matje</span>
   <nav><a href="/demo/matje">Ballina</a><a href="/demo/matje/regjistrohu">Regjistrohu</a></nav>
