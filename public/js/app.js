8cafef45d775bd94d16f8fa40d70df8ba1dde476
EduardiP <blijonline010@gmail.com>
2026-08-09 14:11:20 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -360,6 +360,7 @@ function renderUserMenu(){
   const items = [
     { l:'Profili',           fn:function(){ nav({v:'profile', nav:'profili'}); } },
     { l:'Ekipi & Rolet',     fn:function(){ nav({v:'profile', nav:'ekipi'}); } },
+    { l:'Faturimi & Plani',  fn:function(){ nav({v:'profile', nav:'plani'}); } },
     { l:'Plani',             fn:function(){ nav({v:'profile', nav:'plani'}); } },
     { l:'Ndihmë & Suport',   fn:function(){ nav({v:'profile', nav:'suport'}); } },
     { l:'Cilësimet',         fn:function(){ nav({v:'profile', nav:'cilesimet'}); } },
