4d499c540db3df270b5ae62e151ca22d9d387aa3
EduardiP <blijonline010@gmail.com>
2026-08-20 21:57:16 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -34,8 +34,7 @@ const NAV2 = [
     {l:'Reklamat', nav:'analytics'},
     {l:'Snippet-et', nav:'analytics'},
     {l:'Dhënie', nav:'analytics'},
-    {l:'Marrje', nav:'analytics'},
-    {l:'Deficiti', nav:'analytics'}
+    {l:'Marrje', nav:'analytics'}
   ]},
   { k:'insights', l:'Vështrime', subs:[
     {l:'Pika AI & kombinimet kryesore', nav:'insights'},
