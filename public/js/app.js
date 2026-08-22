c3208c34d4a2ca3a3b1baca024d11d9d72c60be1
EduardiP <blijonline010@gmail.com>
2026-08-22 21:56:02 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2724,7 +2724,7 @@ function stepPershkrimi(b){
     '<div id="d_res" class="hide" style="margin-top:16px;">'+
       '<label>Përmbledhja (e editueshme)</label>'+
       '<textarea id="e_perm"></textarea>'+
-      '<button class="primary" id="e_next" onclick="ruajPershkrim()">Ruaj</button>'+
+      '<button class="primary" id="e_next" onclick="vazhdoPershkrim()">Ruaj</button>'+
       '<div class="msg" id="e_msg"></div>'+
     '</div>';
   if(une.permbledhje){ $('d_res').classList.remove('hide'); $('e_perm').value=une.permbledhje; }
