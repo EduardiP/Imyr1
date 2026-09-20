0fb5367c789dd2898cde7717c7ea2eab50eb6574
EduardiP <blijonline010@gmail.com>
2026-09-20 18:13:39 +0200

    Update demo-dizajn.js

--- demo-dizajn.js
+++ demo-dizajn.js
@@ -39,6 +39,15 @@ function layout(){
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-G3EGWQ4DYD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-G3EGWQ4DYD');
+</script>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>PayFlow HR — Payroll Software</title>
